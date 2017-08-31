036f804db609cd9af3b2b4f1dfdba076cda2320f
r8d8 <ckryvomaz@gmail.com>
2017-08-31 08:24:47 +0300

    Lack of deployment mechanism Added deployment of  CI build into gcloud platform

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+CLI_ARCHIVE_NAME="geth-classic-$TRAVIS_OS_NAME-$(janus version -format='TAG_OR_NIGHTLY')"
+zip "$CLI_ARCHIVE_NAME.zip" emerald
+tar -zcf "$CLI_ARCHIVE_NAME.tar.gz" emerald
+
+mkdir deploy
+mv *.zip *.tar.gz deploy/
+ls -l deploy/
+
+janus deploy -to="builds.etcdevteam.com/go-ethereum/$(janus version -format='v%M.%m.x')/" -files="./deploy/*" -key="./gcloud-travis.json.enc"
