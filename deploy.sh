6ca76899077652afad117fe7fc46575d7f10098e
r8d8 <ckryvomaz@gmail.com>
2017-08-31 17:04:06 +0300

    Invalid path in deployment script Fixed

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-CLI_ARCHIVE_NAME="emerald-cli-$TRAVIS_OS_NAME-$(janus version -format='TAG_OR_NIGHTLY')"
+VERSION_BASE=$(janus version -format='v%M.%m.x')
+CLI_ARCHIVE_NAME="emerald-cli-$TRAVIS_OS_NAME-$VERSION_BASE"
 zip "$CLI_ARCHIVE_NAME.zip" emerald
 tar -zcf "$CLI_ARCHIVE_NAME.tar.gz" emerald
 
@@ -8,4 +9,4 @@ mkdir deploy
 mv *.zip *.tar.gz deploy/
 ls -l deploy/
 
-janus deploy -to="builds.etcdevteam.com/emerald-cli/$(janus version -format='v%M.%m.x')/" -files="./deploy/*" -key="./gcloud-travis.json.enc"
+janus deploy -to="builds.etcdevteam.com/emerald-cli/$VERSION_BASE/" -files="./deploy/*" -key="./gcloud-travis.json.enc"
