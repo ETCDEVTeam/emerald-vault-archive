19ced754082511be8d9ba913fbdcb4e434edda1d
Igor Artamonov <igor@artamonov.ru>
2017-09-04 21:17:52 -0400

    solution: version variable

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
 VERSION_BASE=$(janus version -format='v%M.%m.x')
-CLI_ARCHIVE_NAME="emerald-cli-$TRAVIS_OS_NAME-$VERSION_BASE"
+CLI_ARCHIVE_NAME="emerald-cli-$TRAVIS_OS_NAME-$APP_VERSION"
 zip "$CLI_ARCHIVE_NAME.zip" target/release/emerald
 tar -zcf "$CLI_ARCHIVE_NAME.tar.gz" target/release/emerald
 echo "Deploy to http://builds.etcdevteam.com/emerald-cli/$VERSION_BASE/"
-
 
 mkdir deploy
 mv *.zip *.tar.gz deploy/
