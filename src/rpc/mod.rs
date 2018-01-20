bd955b34881ceba0867d934133af1818a5f9b29f
Constantine <ckryvomaz@gmail.com>
2018-01-20 21:12:30 +0200

    RPC call for gas_price and gas estimation (#67)
    * Update AppVeyor badge
    
    * Fix hd-path parsing
    
    * Bump version to 0.19
    Update emerald-rs to 0.19
    
    * Lack of request to node for gas price and gas estimation
    Added
    
    * Fix source code formatting
    
    * Fixed rpc call handlers for gas estimation and gas price
    
    * Invalid transaction signing
    Fixed, used proper order for attributes assigment in `Transaction`
    
    * Remove beta toolchain from travis config
    
    * Add log info to travis config
    
    * Fix travis config
    
    * Fix unit tests

--- src/rpc/mod.rs
+++ src/rpc/mod.rs
@@ -8,6 +8,7 @@ use jsonrpc_core::{Params, Value};
 use cmd::Error;
 use hex::ToHex;
 use emerald::{trim_hex, Address};
+use cmd::hex_to_32bytes;
 
 /// Get nonce for address from remote node
 ///
@@ -31,26 +32,6 @@ pub fn get_nonce(rpc: &RpcConnector, addr: &Address) -> Result<u64, Error> {
     }
 }
 
-/// Send signed raw transaction to the remote client
-///
-/// # Arguments:
-///
-/// * raw - signed tx
-///
-/// # Return:
-///
-/// * String - transaction hash
-///
-pub fn send_transaction(rpc: &RpcConnector, raw: &[u8]) -> Result<String, Error> {
-    let data = vec![Value::String(format!("0x{}", &raw.to_hex()))];
-    let params = Params::Array(data);
-    rpc.send_post(&MethodParams(ClientMethod::EthSendRawTransaction, &params))
-        .and_then(|v| match v.as_str() {
-            Some(str) => Ok(str.to_string()),
-            None => Err(Error::ExecError("Can't parse tx hash".to_string())),
-        })
-}
-
 /// Get balance for selected account
 ///
 /// # Arguments:
@@ -72,5 +53,52 @@ pub fn get_balance(rpc: &RpcConnector, addr: &Address) -> Result<String, Error> 
         .and_then(|v| match v.as_str() {
             Some(str) => Ok(str.to_string()),
             None => Err(Error::ExecError(format!("Can't get balance for {}", addr))),
+        })
+}
+
+/// Get estimated gas from remote node
+pub fn get_gas(rpc: &RpcConnector) -> Result<u64, Error> {
+    let data = vec![Value::String("latest".to_string())];
+    let params = Params::Array(data);
+    let val = rpc.send_post(
+        &MethodParams(ClientMethod::EthEstimateGas, &params),
+    )?;
+
+    match val.as_str() {
+        Some(s) => Ok(u64::from_str_radix(trim_hex(s), 16)?),
+        None => Err(Error::ExecError("Can't estimate required gas".to_string())),
+    }
+}
+
+/// Get gas price from remote node
+pub fn get_gas_price(rpc: &RpcConnector) -> Result<[u8; 32], Error> {
+    let params = Params::Array(vec![]);
+    let val = rpc.send_post(
+        &MethodParams(ClientMethod::EthGasPrice, &params),
+    )?;
+
+    match val.as_str() {
+        Some(s) => Ok(hex_to_32bytes(trim_hex(s))?),
+        None => Err(Error::ExecError("Can't estimate gas price".to_string())),
+    }
+}
+
+/// Send signed raw transaction to the remote client
+///
+/// # Arguments:
+///
+/// * raw - signed tx
+///
+/// # Return:
+///
+/// * String - transaction hash
+///
+pub fn send_transaction(rpc: &RpcConnector, raw: &[u8]) -> Result<String, Error> {
+    let data = vec![Value::String(format!("0x{}", &raw.to_hex()))];
+    let params = Params::Array(data);
+    rpc.send_post(&MethodParams(ClientMethod::EthSendRawTransaction, &params))
+        .and_then(|v| match v.as_str() {
+            Some(str) => Ok(str.to_string()),
+            None => Err(Error::ExecError("Can't parse tx hash".to_string())),
         })
 }
