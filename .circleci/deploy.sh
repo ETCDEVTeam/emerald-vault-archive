20f246c874432413a38addc8fb13fb1362eda32e
r8d8 <ckryvomaz@gmail.com>
2018-09-26 16:03:56 +0300

    Update CI configs with new name for binary

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -3,15 +3,15 @@
 set -e
 
 VERSION_BASE=$(janus version -format='v%M.%m.x')
-CLI_ARCHIVE_NAME="emerald-cli-osx-$APP_VERSION"
+CLI_ARCHIVE_NAME="emerald-vault-osx-$APP_VERSION"
 mv target/release/emerald ./emerald
 zip -j "$CLI_ARCHIVE_NAME.zip" emerald
 tar -zcf "$CLI_ARCHIVE_NAME.tar.gz" emerald
-echo "Deploy to http://builds.etcdevteam.com/emerald-cli/$VERSION_BASE/"
+echo "Deploy to http://builds.etcdevteam.com/emerald-vault/$VERSION_BASE/"
 
 mkdir deploy
 mv *.zip *.tar.gz deploy/
 ls -l deploy/
 
-janus deploy -to="builds.etcdevteam.com/emerald-cli/$VERSION_BASE/" -files="deploy/*" -key=".circleci/gcloud-circleci.json.enc" -gpg
+janus deploy -to="builds.etcdevteam.com/emerald-vault/$VERSION_BASE/" -files="deploy/*" -key=".circleci/gcloud-circleci.json.enc" -gpg
 echo "Deployed"
