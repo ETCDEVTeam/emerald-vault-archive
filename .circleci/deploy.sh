4a7771a0869b62fb8aed39b82721d526938648e7
Igor Artamonov <igor@artamonov.ru>
2019-03-19 19:52:41 -0400

    problem: macos builds in a wrong path

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -3,15 +3,15 @@
 set -e
 
 VERSION_BASE=$(janus version -format='v%M.%m.x')
-CLI_ARCHIVE_NAME="emerald-vault-osx-$APP_VERSION"
-mv target/release/emerald-vault ./emerald-vault
-zip -j "$CLI_ARCHIVE_NAME.zip" emerald-vault
-tar -zcf "$CLI_ARCHIVE_NAME.tar.gz" emerald-vault
-echo "Deploy to http://builds.etcdevteam.com/emerald-vault/$VERSION_BASE/"
+CLI_ARCHIVE_NAME="emerald-cli-mac-$APP_VERSION"
+mv target/release/emerald-vault ./emerald
+zip -j "$CLI_ARCHIVE_NAME.zip" emerald
+tar -zcf "$CLI_ARCHIVE_NAME.tar.gz" emerald
+echo "Deploy to http://builds.etcdevteam.com/emerald-cli/$VERSION_BASE/"
 
 mkdir deploy
 mv *.zip *.tar.gz deploy/
 ls -l deploy/
 
-janus deploy -to="builds.etcdevteam.com/emerald-vault/$VERSION_BASE/" -files="deploy/*" -key=".circleci/gcloud-circleci.json.enc" -gpg
+janus deploy -to="builds.etcdevteam.com/emerald-cli/$VERSION_BASE/" -files="deploy/*" -key=".circleci/gcloud-circleci.json.enc" -gpg
 echo "Deployed"
